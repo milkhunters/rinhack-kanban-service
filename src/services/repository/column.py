48bd5f94c1c45c22409e93d729971d70187989a8
JKearnsl <boyarshinovn@gmail.com>
2023-11-26 00:57:00 +0300

    Добавлена подгрузка тегов

--- src/services/repository/column.py
+++ src/services/repository/column.py
@@ -12,9 +12,17 @@ class ColumnRepo(BaseRepository[tables.Column]):
     table = tables.Column
 
     async def get(self, **kwargs) -> tables.Column:
-        stmt = select(self.table).filter_by(**kwargs).options(subqueryload(self.table.tasks))
+        stmt = select(
+            self.table
+        ).filter_by(**kwargs).options(
+            joinedload(self.table.tasks).joinedload(tables.Task.tags)
+        )
         return (await self._session.execute(stmt)).scalars().first()
 
     async def get_all(self, **kwargs) -> Sequence[Column]:
-        stmt = select(self.table).filter_by(**kwargs).options(subqueryload(self.table.tasks))
-        return (await self._session.execute(stmt)).scalars().all()
+        stmt = select(
+            self.table
+        ).filter_by(**kwargs).options(
+            joinedload(self.table.tasks).joinedload(tables.Task.tags)
+        )
+        return (await self._session.execute(stmt)).unique().scalars().all()
